07e73fed7b0ce468405d8eca54d04566bc3a2682
Hamad Zia <77169231+hamadzia74@users.noreply.github.com>
2022-06-14 01:12:34 +0500

    Add files via upload

---
+++ The Pass4Sure Project/p4scode.php
@@ -0,0 +1,218 @@
+<html>
+<head>
+<title>The Pass4Sure</title>
+<link href="_assets/css/style.css" rel="stylesheet" type="text/css"/> 
+<link href="_assets/image/icon.png" rel="shortcut icon"/>
+</head>
+
+<body>
+<div id="outer-wrapper-head-back">
+<div id="container-head-back">
+<div id="logo-div">
+<img src="_assets/image/logo.png" />
+</div>
+<div id="search-div">
+<input type="text" placeholder="Search..." id="search-set" />
+<img src="_assets/image/search-logo.png" id="search-icon-set" />
+<input type="submit" value="Go" id="go-btn-set" />
+</div>
+
+</div>
+</div>
+
+<div id="outer-wrapper-navbar">
+<div id="container-navbar">
+<ul id="navbar-l">
+<li><a href="#">Home</a></li>
+<li><a href="#">Products</a></li>
+<li><a href="#">Certifications</a></li>
+<li><a href="#">Bundles</a></li>
+<li><a href="#">Partners</a></li>
+<li><a href="#">Contact us</a></li>
+</ul>
+</div>
+</div>
+
+<div id="outer-wrapper-slider">
+<div id="container-slider">
+<div id="left-slider-div">
+<p id="first-paragraph-slider">Passing Certification Exam Made Easy</p>
+<p id="second-paragraph-slider">Everything you need to take, Prepare and
+quickly pass the tough Certification Exam</p>
+<ul>
+<li>Simple - Instan Download!</li>
+<li>Unparalleled - 24/7 Customer Support</li>
+<li>Professional - Free 30 Days Update</li>
+<li>Experienced- Over 80,000 Satisfied Customers</li>
+</ul>
+</div>
+<div id="right-slider-div">
+<div id="Exam-Request-image" ><img src="_assets/image/Exam-Request-image.png"/></div>
+<form><input type="text" placeholder="Enter Exam Coad" class="input-area"/>
+<input type="text" placeholder="Enter Exam Coad" class="input-area"/>
+<input type="text" placeholder="Enter Email ID" class="input-area"/>
+<input type="submit" value="Submit" id="Exam-Request-btn-set"/>
+</form>
+</div>
+</div>
+</div>
+<div id="outer-wrapper-content-back">
+<div id="container-content-back">
+<div id="outer-wrapper-content-navbar">
+<ul id="outer-wrapper-content-navbar-ul">
+<li>cisco</li>
+<li>microsoft</li>
+<li>comptia</li>
+<li>vmware</li>
+<li>pmi</li>
+<li>pgmp</li>
+</ul>
+<div id="content-first-box" class="content-box">
+<p class="content-header-box">success stories</p>
+<ul id="content-first-box-ul">
+<li>Fast service</li>
+<li>Great helpdesk</li>
+<li>Pay safe</li>
+<li>Many telephone providers</li>
+<li>Also available in weekends</li>
+<li>Fast service</li>
+<li>Great helpdesk</li>
+<li>Pay safe</li>
+<li>Many telephone providers</li>
+<li>Also available in weekends</li>
+</ul>
+<input type="submit" value="Show all benefits!" id="content-first-box-button"/>
+</div>
+<div id="content-second-box" class="content-box">
+<p class="content-header-box">money back guarantee</p>
+<div id="content-second-box-text-para">Pass4sure has a remarkable Candi-date Success record.
+ We're confident of our products and provide a no hassle money back guarantee.
+ Should you fail your exam after buying our products we will refund the full amount back to you.
+ Thats how confident we are!</div>
+<div id="content-second-box-ul-div">
+<ul>
+<li id="content-second-box-outer-li">
+Money Back Guarantee
+<ul id="content-second-box-inner-ul-div">
+<li>Fast service</li>
+<li>Great helpdesk</li>
+<li>Pay safe</li>
+<li>Fast service</li>
+<li>Pay safe</li>
+</ul></li>
+</ul>
+</div>
+<div id="content-second-box-inner-image-div"></div>
+<input type="submit" value="Show all packages!" id="content-second-box-button"/>
+</div>
+<div id="content-third-box" class="content-box">
+<p class="content-header-box">top certifications</p>
+<div id="content-third-box-text-para">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ante sapien, vehicula ut sagittis sed, placerat quis lectus.</div>
+<div id="content-third-box-first-ul-div">
+<ul>
+<li id="content-third-box-outer-li">
+<span>Cisco</span>
+<ul class="content-third-box-inner-ul-div">
+<li>CCNA</li>
+<li>CCNP</li>
+<li>CCIE(step I &II)</li>
+</ul>
+</li>
+<li id="content-third-box-outer-li">
+<span>Cisco</span>
+<ul class="content-third-box-inner-ul-div">
+<li>CCNA</li>
+<li>CCNP</li>
+<li>CCIE(step I &II)</li>
+</ul>
+</li>
+<li id="content-third-box-outer-li">
+<span>Cisco</span>
+<ul class="content-third-box-inner-ul-div">
+<li>CCNA</li>
+<li>CCNP</li>
+<li>CCIE(step I &II)</li>
+</ul>
+</li>
+</ul>
+</div>
+<div id="content-third-box-second-ul-div">
+<ul>
+<li id="content-third-box-outer-li">
+<span>Cisco</span>
+<ul class="content-third-box-inner-ul-div">
+<li>CCNA</li>
+<li>CCNP</li>
+<li>CCIE(step I &II)</li>
+</ul></li>
+<li id="content-third-box-outer-li">
+<span>Cisco</span>
+<ul class="content-third-box-inner-ul-div">
+<li>CCNA</li>
+<li>CCNP</li>
+<li>CCIE(step I &II)</li>
+</ul>
+</li>
+<li id="content-third-box-outer-li">
+<span>Cisco</span>
+<ul class="content-third-box-inner-ul-div">
+<li>CCNA</li>
+<li>CCNP</li>
+<li>CCIE(step I &II)</li>
+</ul>
+</li>
+</ul></div>
+</div>
+</div>
+</div>
+</div>
+<div id="outer-wrapper-first-footer-back">
+<div id="container-first-footer-back">
+<div class="first-footer-inner-divs">
+<p style="font-size:16px;font-family:Arial;color:white; font-weight:bold;">Exam Training<hr style="width:114px;"/>
+</p>
+<p>home
+<br/>products
+<br/>bundles
+<br/>online Courses
+<br/>frequently asked questions</p>
+</div>
+<div class="first-footer-inner-divs">
+<p style="font-size:16px;font-family:Arial;color:white; font-weight:bold;">The pass 4 sure site<hr style="width:155px;"/>
+</p>
+<p>demo
+<br/>success stories
+<br/>my account
+<br/>about us<br/>
+contact us</p></div>
+<div class="first-footer-inner-divs">
+<p style="font-size:16px;font-family:Arial;color:white; font-weight:bold;">Useing pass 4 sure<hr style="width:150px;"/>
+</p>
+<p>guarantee
+<br/>terms and conditions
+<br/>privacy statement
+<br/>careers at p4s
+<br/>sitemap 12345</p></div>
+<div class="first-footer-inner-divs">
+<p style="font-size:16px;font-family:Arial;color:white; font-weight:bold;">Patnering<hr style="width:150px;"/>
+</p>
+<p>site licensing
+<br/>affiliates
+<br/>resellers</p></div>
+<div class="first-footer-inner-divs">
+<p style="font-size:16px;font-family:Arial;color:white; font-weight:bold;">Site Purchases<hr style="width:150px;"/>
+</p>
+<p>Any charges made through this site will appear as Pass4sure.com. Trademarks are the property of their respective owners.</p>
+</div>
+</div>
+</div>
+<div id="outer-wrapper-second-footer-back">
+<div id="container-second-footer-back">
+<div id="second-footer-inner-div">
+<p class="second-footer-inner-text">(C) 2012 www.thepass4sure.us | Designed and Developed by Technolangs</p>
+<p class="second-footer-inner-text">Thepass4sure.us material do not contain actual questions and answers of any certification.</p></div>
+</div>
+</div>
+</body>
+</html>
